51ef60edd7be01942e09b96befda41a766f6c411
lifelikeaboat <40230555+lifelikeaboat@users.noreply.github.com>
2018-06-14 02:12:28 -0700

    some updates
    to make program more readable

--- test-yield.py
+++ test-yield.py
@@ -1,8 +1,8 @@
-def main(i=1,j=10,k=[]):
-	if i==j:
+def main(start=1,sum=10,k=[]):
+	if start==sum:
 		yield k
-	for i in list(range(1,j+1)):
-		if  i>j or (len(k)>0 and k[-1]>=i):
+	for start in list(range(1,sum+1)):
+		if   (len(k)>0 and k[-1]>=start):
 			continue
-		for x in main(i=i+1,j=j,k=k+[i]):
-			yield x
+		for result in main(start=start+1,sum=sum,k=k+[start]):
+			yield result
